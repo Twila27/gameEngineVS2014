bf0de170254b5006f18f8576812c360c36a9bf9c
Benjamin Gibson <benjadg@okstate.edu>
2014-11-11 19:36:56 -0600

    Sprint 4 Checkpoint: Scripting Basics Done

--- code/Scripts.cpp
+++ code/Scripts.cpp
@@ -4,7 +4,7 @@ void MoverScript::initProperty(FILE *F, const string& propertyName, const string
 	if (name == "velocity") getFloats(F, &velocity[0], 3);
 }
 bool MoverScript::setProperty(const string& propertyName, const string& propertyVal) {
-	if (name == "velocity") return sscanf(propertyVal.c_str(), "[%f,%f,%f]", &velocity.x, &velocity.y, &velocity.z);
+	if (propertyName == "velocity") return sscanf(propertyVal.c_str(), "[%f,%f,%f]", &velocity.x, &velocity.y, &velocity.z);
 }
 void MoverScript::update(Camera& cam, float dt) {
 	assert(node != nullptr);
